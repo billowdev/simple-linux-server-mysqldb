cb58e62b5f7d4cb825e65335f4e5ec22985009dd
lacakp <lacakkarapon@gmail.com>
2021-10-08 20:17:38 +0700

    update

--- conectdb.php
+++ conectdb.php
@@ -1,7 +1,7 @@
 <!-- connectdb.php -->
 
 <?php
-$con= mysqli_connect("192.168.xx.xx","usernamedb","","thisisdb") or die("Error: " . mysqli_error($con));
+$con= mysqli_connect("192.168.xx.xx","usernamedb","password","thisisdb") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
 error_reporting( error_reporting() & ~E_NOTICE );
 date_default_timezone_set('Asia/Bangkok');
