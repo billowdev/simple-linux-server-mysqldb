81bcb51c95e47680bd4524fb1c15dbaeb0aeca7a
lacakp <lacakkarapon@gmail.com>
2021-10-08 21:09:47 +0700

    update member.php

--- login.php
+++ login.php
@@ -8,15 +8,12 @@ include('pull_bootstrap.php');
 #btn{
 width:100%;
 }
-#sbody{
-  background-image: url("https://github.com/lacakp/lacakp/blob/2af8b45283fe90ff7fe223eba623c1f7c918163c/Images/t_Panda.gif");
-  background-color: #89ADCB;
-}
 </style>
-<div class="container sbody" style="padding-top:100px">
+
+<div class="container" style="padding-top:100px">
   <div class="row">
     <div class="col-md-4"></div>
-    <div class="col-md-4">
+    <div class="col-md-4" style="background-color: #89ADCB">
       <h3 align="center">
       <span class="glyphicon glyphicon-lock"> </span>
       Form Login </h3>
--- member.php
+++ member.php
@@ -10,18 +10,32 @@ include('connectdb.php');
  	if($Status!='USER'){
     Header("Location: ../logout.php");  
   }  
+  include('pull_bootstrap.php');
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Member-Page</title>
-</head>
-<body>
-	<form action="logout.php">
-	<h1>หน้าสมาชิก</h1>
-	<h3> สวัสดี คุณ <?php echo $name; ?> สถานะ <?php echo $level; ?> </h3>
-	<input type="submit" value="ออกจากระบบ">
-	</form>
+
+<style type="text/css">
+#btn{
+width:100%;
+}
+</style>
+
+<div class="container" style="padding-top:100px">
+  <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4" style="background-color: #89ADCB">
+      <h3 align="center">
+      <span class="glyphicon glyphicon-lock"> </span>
+      Member - Page </h3>
+		<form action="logout.php">
+		<h1>หน้าสมาชิก</h1>
+		<h3> สวัสดี คุณ <?php echo $name; ?> สถานะ <?php echo $level; ?> </h3>
+		<input type="submit" value="ออกจากระบบ">
+		</form>
+
+    </div>
+</div>
+</div>
 </body>
 </html>
