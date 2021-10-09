cad111351828bf406cf76b05092e3165f9bed8c7
lacakp <lacakkarapon@gmail.com>
2021-10-09 12:04:39 +0700

    update

--- connectdb.php
+++ connectdb.php
@@ -2,12 +2,12 @@
 
 <?php
 
-$dbServerName = "192.168.48.133"; // ip address (hostname -I) : example 192.168.48.133
-$dbUsername = "testuser"; // username example : phpmyadmin
-$dbPassword = "root1234"; // db pass exsample : codetopanda123456789
-$dbName = "mydatabase"; // your database to connect
+// ip address (hostname -I) : example 192.168.48.133
+// username example : phpmyadmin
+// db pass exsample : codetopanda123456789
+// your database to connect
 
-$con= mysqli_connect($dbServerName,$dbUsername,$dbPassword,$dbName) or die("Error: " . mysqli_error($con));
+$con= mysqli_connect("192.168.48.133", "testuser", "root1234", "mydatabase") or die("Error ! : " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
 error_reporting( error_reporting() & ~E_NOTICE );
 date_default_timezone_set('Asia/Bangkok');
